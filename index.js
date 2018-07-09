381b1c3a34bb54bee0a07cc228c26e9f58112184
TechnocomTnc <predapon.t@gmail.com>
2018-07-09 17:28:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -32,28 +32,24 @@ var dbConfig = {
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.get('/webhook', (req, res) => {
+app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    reply(reply_token,msg)    
+    reply(reply_token, msg)
     res.sendStatus(200)
 })
-
-
 app.listen(port)
-function reply(reply_token,msg) {
+function reply(reply_token, msg) {
     let headers = {
         'Content-Type': 'application/json',
         'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
     }
     let body = JSON.stringify({
-
         replyToken: reply_token,
         messages: [{
             type: 'text',
             text: msg
         }]
-        
     })
     request.post({
         url: 'https://api.line.me/v2/bot/message/reply',
@@ -63,5 +59,3 @@ function reply(reply_token,msg) {
         console.log('status = ' + res.statusCode);
     });
 }
-
-
