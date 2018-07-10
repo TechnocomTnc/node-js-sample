10f69b1ca06738534116c60577de977aa7d8f1ec
TechnocomTnc <predapon.t@gmail.com>
2018-07-10 09:22:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function reply(reply_token, msg) {
     conn.connect().then(function () {
         var req = new sql.Request(conn);
             
-            req.query('SELECT * FROM Question ', function(err, rows) {
+            req.query('SELECT * FROM Question WHERE q_topic = ' + msg, function(err, rows) {
                 if (err) {
                     throw err;
                     console.error(err);
