5ef94960bcbf57aec55ae4414b8b7bc7e01daa81
TechnocomTnc <predapon.t@gmail.com>
2018-07-10 17:23:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -139,7 +139,7 @@ function groupMs(reply_token, gid,msg){
                         var conn = new sql.ConnectionPool(dbConfig);
                             conn.connect().then(function () {
                                 var req = new sql.Request(conn);
-//                                 req.query("INSERT INTO [dbo].[groupName] ([groupID],[Gname]) VALUES ('" + gid + "','" + Ngroup + "')")
+                                req.query("INSERT INTO [dbo].[groupName] ([groupID],[Gname]) VALUES ('" + gid + "','" + Ngroup + "')")
                                 
                                 req.query("CREATE TABLE [dbo].["+ Ngroup +"]([m_Id] [int] IDENTITY(1,1) NOT NULL,[UID] [varchar](500) NULL,[Mesg] [varchar](500) NULL)")
                                 
