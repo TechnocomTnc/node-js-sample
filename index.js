ec4c4200601c3b843e17b15f0d50c30478c7e75d
TechnocomTnc <predapon.t@gmail.com>
2018-07-10 11:01:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    var msg = req.body.events[0].message.text
-    var sender = req.body.events[0].source.groupId
+    let msg = req.body.events[0].message.text
+    let sender = req.body.events[0].source.groupId
     reply(reply_token, msg, sender)
     res.sendStatus(200)
 })
@@ -37,36 +37,57 @@ function reply(reply_token, msg, sender) {
     var conn = new sql.ConnectionPool(dbConfig);
     conn.connect().then(function () {
         var req = new sql.Request(conn);
-            req.query('SELECT * FROM Question WHERE q_topic = msg', function(rows) {        
-                QID = rows.recordset[0].q_Id
-                req.query('SELECT * FROM Answer WHERE a_Id = QID', function(row) {
-                        arrName = row.recordset[0].a_topic 
-
-                        let headers = {
-                            'Content-Type': 'application/json',
-                            'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
+            req.query('SELECT * FROM Question', function(err, rows) {
+                if (err) {
+                    throw err;
+                    console.error(err);
+                    conn.close();  
+                }else{                  
+                        for (var i=0;i<rows.rowsAffected;i++){
+                            if(rows.recordset[i].q_topic == msg){
+                                QID = rows.recordset[i].q_Id
+                                break                          
+                            }else {
+                                arrName = '\nNOT FOUND'
+                                QID = null
+                            
+                            }
                         }
-                        let body = JSON.stringify({
-                            replyToken: reply_token,
-                            messages: [{
-                                    type: 'text',
-                                    text: arrName
-                                },
-                                {
-                                    type: 'text',
-                                    text: 'UserId '+ sender
-                                }]
+                        req.query('SELECT * FROM Answer WHERE a_Id ='+ QID, function(err, row) {
+                            if (err) {
+                                throw err;
+                                console.error(err);
+                                conn.close();  
+                            }else{
+                                arrName = row.recordset[0].a_topic 
+
+                                let headers = {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
+                                }
+                                let body = JSON.stringify({
+                                    replyToken: reply_token,
+                                    messages: [{
+                                            type: 'text',
+                                            text: arrName
+                                        },
+                                        {
+                                            type: 'text',
+                                            text: 'UserId '+ sender
+                                        }]
+                                })
+                                request.post({
+                                    url: 'https://api.line.me/v2/bot/message/reply',
+                                    headers: headers,
+                                    body: body
+                                }, (err, res, body) => {
+                                    console.log('status = ' + res.statusCode);
+                                });
+                                conn.close(); 
+                            }
                         })
-                        request.post({
-                            url: 'https://api.line.me/v2/bot/message/reply',
-                            headers: headers,
-                            body: body
-                        }, (err, res, body) => {
-                            console.log('status = ' + res.statusCode);
-                        });
-                        conn.close(); 
-                })
-            
+                    
+                }
             });
 
 
