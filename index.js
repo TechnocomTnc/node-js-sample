390ba23ddb130558b48b10c46ebbfad8bfe51ae6
TechnocomTnc <predapon.t@gmail.com>
2018-07-11 16:19:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,11 +28,31 @@ app.use(bodyParser.json())
 
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
-    let msg = req.body.events[0].message.text
+    let msgtype = req.body.events[0].message.type
     let gid = req.body.events[0].source.groupId
     let uid = req.body.events[0].source.userId
 
-    reply(reply_token, msg)
+    if (msgtype == 'String'){
+        let headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
+        }
+        let body = JSON.stringify({
+            replyToken: reply_token,
+            messages: [{
+                    type: 'text',
+                    text: 'sssss'
+                }]
+        })
+        request.post({
+            url: 'https://api.line.me/v2/bot/message/reply',
+            headers: headers,
+            body: body
+        }, (err, res, body) => {
+            console.log('status = ' + res.statusCode);
+        });
+
+    }
     if(gid != null)
        groupMs(uid,gid,msg)
     
