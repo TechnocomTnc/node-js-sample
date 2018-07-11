3f261858246a481ad382430262f835b85b94f3e0
TechnocomTnc <predapon.t@gmail.com>
2018-07-11 16:08:36 +0700

    Update index.js

--- index.js
+++ index.js
@@ -29,33 +29,12 @@ app.use(bodyParser.json())
 app.post('/webhook', (req, res) => {
     let reply_token = req.body.events[0].replyToken
     let msg = req.body.events[0].message.text
-    let evj = req.body.events[0].type
     let gid = req.body.events[0].source.groupId
     let uid = req.body.events[0].source.userId
 
-    let headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
-    }
-    let body = JSON.stringify({      
-        // replyToken: reply_token,
-        to:'U44c2e72994ecff53947def77dbe79b62',
-        messages: [{
-                type: 'text',
-                text: req.body.events[0].type
-            }]
-    })
-    request.post({
-        url: 'https://api.line.me/v2/bot/message/push',
-        headers: headers,
-        body: body
-    });
-
-    
-
-    // reply(reply_token, msg)
-    // if(gid != null)
-    //    groupMs(uid,gid,msg)
+    reply(reply_token, msg)
+    if(gid != null)
+       groupMs(uid,gid,msg)
     
     res.sendStatus(200)
 })
@@ -105,7 +84,7 @@ function reply(reply_token, msg) {
                                     // }, (err, res, body) => {
                                     //     console.log('status = ' + res.statusCode);
                                     // });
-                                    conn.close(); 
+                                    //conn.close(); 
                                 }
                             })
                         }else{
@@ -127,7 +106,7 @@ function reply(reply_token, msg) {
                             // }, (err, res, body) => {
                             //     console.log('status = ' + res.statusCode);
                             // });
-                            conn.close(); 
+                            //conn.close(); 
 
                         }
                     
