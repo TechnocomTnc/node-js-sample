23fcebcc94fbeb7cbab41a801cad19cc51eb7c6e
TechnocomTnc <predapon.t@gmail.com>
2018-07-11 10:30:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -130,26 +130,6 @@ function groupMs(reply_token, gid,msg){
                     for(var i=0;i<rows.rowsAffected;i++){
                         if(rows.recordset[i].groupID == gid){
                             
-                            let headers = {
-                                'Content-Type': 'application/json',
-                                'Authorization': 'Bearer {7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU=}'
-                            }
-                            let body = JSON.stringify({
-                                replyToken: reply_token,
-                                messages: [{
-                                        type: 'text',
-                                        text: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-                                    }]
-                            })
-                            request.post({
-                                url: 'https://api.line.me/v2/bot/message/reply',
-                                headers: headers,
-                                body: body
-                            }, (err, res, body) => {
-                                console.log('status = ' + res.statusCode);
-                            });
-
-
                            flag = 1
                             break
                         }else flag = 0
@@ -193,7 +173,7 @@ function groupMs(reply_token, gid,msg){
                         conn.connect().then(function () {
                             var req = new sql.Request(conn);
                             //req.query("CREATE TABLE [dbo].[Boardgame_"+ gid +"]([m_Id] [int] IDENTITY(1,1) NOT NULL,[UID] [varchar](500) NULL,[Mesg] [varchar](500) NULL,CONSTRAINT [m_Id] PRIMARY KEY CLUSTERED([m_Id] ASC)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]) ON [PRIMARY]")
-                            req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUE ('" + gid + "','" + msg + "')")
+                            req.query("INSERT INTO [dbo].["+ gid +"] ([UID],[Mesg]) VALUES ('" + gid + "','" + msg + "')")
                             
                             let headers = {
                                 'Content-Type': 'application/json',
