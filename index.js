708085f6bd141a58a8de8ab4441d6b0c639ca6e9
TechnocomTnc <predapon.t@gmail.com>
2018-07-12 16:06:20 +0700

    Update index.js

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.post('/webhook', (req, res) => {
                 replyToken: reply_token,
                 messages: [{
                         type: 'text',
-                        text: binary
+                        text: typeof binary
                     }
                 ]
             })
