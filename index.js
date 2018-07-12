f0d6bb67d5ba7310af08cf24a93b1f08c7cecafd
TechnocomTnc <predapon.t@gmail.com>
2018-07-12 10:54:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', (req, res) => {
     let gid = req.body.events[0].source.groupId
     let uid = req.body.events[0].source.userId
     var msID = req.body.events[0].message.id
-
+    var str = ""
     const client = new line.Client({
         channelAccessToken: '7YR60AJ855Zu1Etxsc7aCdFqhip1o8yAKj7PzLe90ClE9Po0fz5o81BeghtpCki4+zFZ7FrYjjbrFvQw84+Axi+P1zWPnxSCTl/lF5gVTDaDqdC5IHk30qnjo7GQ1hHKizexgGNpBPn/Fwz3slJqkQdB04t89/1O/w1cDnyilFU'
       });
