0bad7e4b1bba6bb826250f7c4c66f56e29f18c8e
TechnocomTnc <predapon.t@gmail.com>
2018-07-17 10:31:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -287,7 +287,10 @@ function handleText(message, replyToken, source) {
       }
     default:
       console.log(`Echo message to ${replyToken}: ${message.text}`);
-      return replyText(replyToken, message.type);
+      return replyText(replyToken, {
+          type: 'text',
+          text: 'ssd'
+        });
   }
 }
 
